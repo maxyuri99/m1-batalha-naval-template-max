c489198da244346a028638ce07682c7636a5e299
maxyuri99 <maxyurisantos@hotmail.com>
2023-05-03 10:16:02 -0300

    Novo commit

--- script.js
+++ script.js
@@ -11,27 +11,19 @@ const seaBoard = [
   Desenvolva seu código logo abaixo
 */
 
-function shipPositions(){
-
-}
-
-function initialBoard(){
-
-}
-
 function allocateShips(shipPositions, initialBoard) {
   for (let i = 0; i < shipPositions.length; i++) {
-    const [row, col] = shipPositions[i];
-    initialBoard[row][col] = "S";
+    const [shipsLine, shipsColumn] = shipPositions[i];
+    initialBoard[shipsLine][shipsColumn] = "S";
   }
   return initialBoard;
 }
 
 function checkGuesses(guesses, mountedBoard) {
   for (let i = 0; i < guesses.length; i++) {
-    const [row, col] = guesses[i];
-    if (mountedBoard[row][col] === "S") {
-      mountedBoard[row][col] = "X";
+    const [guessesLine, gessesColumn] = guesses[i];
+    if (mountedBoard[guessesLine][gessesColumn] === "S") {
+      mountedBoard[guessesLine][gessesColumn] = "X";
     }
   }
   return mountedBoard;
